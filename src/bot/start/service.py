e93ec8898581ec7035c8f1fc86769d9ffb3fb5e1
Egor Kondratev <zhora_kondratev@bk.ru>
2026-02-27 00:43:43 +0300

    feat: new start

--- src/bot/start/service.py
+++ src/bot/start/service.py
@@ -1,8 +1,20 @@
+import random
+
 from aiogram import types
 from aiogram.fsm.context import FSMContext
 
 from src.bot.start.dao import StartDAO
 from src.bot.start.keyboards import StartKeyboard
+
+START_AGAIN_MESSAGES = [
+    "С возвращением! 👋",
+    "Рады видеть тебя снова 😎",
+    "Привет ещё раз 😉",
+    "С возвращением в Приватка VPN 🚀",
+    "И снова ты — отличное решение 😌",
+    "С любовью Приватка ❤️",
+    "Приватка VPN снова на связи 💜",
+]
 
 
 class StartService:
@@ -10,24 +22,37 @@ class StartService:
         self._dao = dao
         self._keyboard = keyboard
 
-    async def start(self, message: types.Message, state: FSMContext):
+    async def start(self, message: types.Message, state: FSMContext, with_smile: bool = True):
         current_state = await state.get_state()
         if current_state is not None:
             await state.clear()
 
-        if not await self._dao.exists_user(telegram_id=message.from_user.id):
-            await self._dao.create_user(telegram_user=message.from_user)
+        if with_smile:
+            await message.answer(
+                "✨",
+                reply_markup=await self._keyboard.start_reply_keyboard(),
+            )
 
-        await message.answer(
-            "✨",
-            reply_markup=await self._keyboard.start_reply_keyboard(),
-        )
-        await message.answer(
-            f"<b>Добро пожаловать в Приватка VPN!</b>\n\n"
+        msg_text = (
+            f"{random.choice(START_AGAIN_MESSAGES)}\n\n"
             f"⚡️ Быстро.\n"
             f"🌍 Без ограничений.\n"
             f"🛡 Без логов.\n\n"
-            f"Выбирай тариф и подключайся за 1 минуту 👇",
+            f"Выбирай тариф и подключайся за 1 минуту 👇"
+        )
+
+        if not await self._dao.exists_user(telegram_id=message.from_user.id):
+            msg_text = (
+                f"<b>Добро пожаловать в Приватка VPN!</b>\n\n"
+                f"⚡️ Быстро.\n"
+                f"🌍 Без ограничений.\n"
+                f"🛡 Без логов.\n\n"
+                f"Выбирай тариф и подключайся за 1 минуту 👇"
+            )
+            await self._dao.create_user(telegram_user=message.from_user)
+
+        await message.answer(
+            text=msg_text,
             parse_mode="html",
             reply_markup=await self._keyboard.start_inline_keyboard(),
         )
--- src/bot/stop_fsm/service.py
+++ src/bot/stop_fsm/service.py
@@ -1,8 +1,13 @@
 from aiogram import types
 from aiogram.fsm.context import FSMContext
 
+from src.bot.start.service import StartService
+
 
 class StopFSMService:
+    def __init__(self, start_service: StartService):
+        self._start_service = start_service
+
     async def stop_fsm(self, message: types.Message, state: FSMContext):
         current_state = await state.get_state()
         if current_state is None:
@@ -10,3 +15,4 @@ class StopFSMService:
             return
         await state.clear()
         await message.answer("Действие отменено")
+        await self._start_service.start(message=message, state=state, with_smile=False)
